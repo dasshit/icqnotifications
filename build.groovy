1a0132f042109082264eb940a9f036c5c03cbda8
v.korobov <v.korobov@corp.mail.ru>
2022-05-22 20:40:18 +0300

    Refactoring

--- build.groovy
+++ build.groovy
@@ -1,16 +1,46 @@
 pipeline {
     agent any
-
+    tools {
+        maven 'apache-maven-3.0.1'
+    }
+    timeout(unit: 'MINUTES', time: 10)
     stages {
         stage('Start') {
+            failFast true
+            parallel {
+                stage("Start notification to VK Teams") {
+                    steps {
+                        def MESSAGE = """Started build $BUILD_DISPLAY_NAME for project: <a href="$GIT_URL">\"$JOB_BASE_NAME\"</a>
+
+Branch: $GIT_BRANCH
+Last commit:
+$GIT_COMMIT - @[$GIT_COMMITTER_EMAIL]"""
+
+                        imSendMessage(MESSAGE: MESSAGE, CHAT_ID: "v.korobov@corp.mail.ru")
+
+                    }
+                }
+                stage('Start checking project directory') {
+                    steps {
+                        sh "set"
+                        sh "pwd"
+                        sh "ls -l"
+                    }
+                }
+            }
+        }
+        stage('Build'){
             steps {
-                imSendMessage(MESSAGE: "Build Step!\n\nWORKSPACE: $WORKSPACE", CHAT_ID: "v.korobov@corp.mail.ru")
+                sh 'mvn --version'
+                sh 'mvn package'
             }
         }
     }
     post {
         always {
-            imSendMessage(MESSAGE: "Post Build Step!\n\nWORKSPACE: $WORKSPACE", CHAT_ID: "v.korobov@corp.mail.ru")
+            sh "set"
+            archiveArtifacts artifacts: "target/*"
+            imSendMessage(MESSAGE: "Build completed", CHAT_ID: "v.korobov@corp.mail.ru")
         }
     }
 }
